a387f1b51132fb2991a4f68ef188c43e9eb5e78a
bsekiewicz <b.sekiewicz@erkakrakow.pl>
2017-03-11 16:40:09 +0100

    parametryzacja

--- R/mail_merge.R
+++ R/mail_merge.R
@@ -3,6 +3,12 @@ library(rmarkdown)
 # devtools::install_github("rpremraj/mailR") # wymaga instalacji java jdk 32bit
 library(mailR)
 
+email_from <- ""
+email_to   <- ""
+subject <- "Potwierdzenie rejestracji na warsztaty"
+login_email <- "xxx@gmail.com"
+login_pass  <- "xxx"
+
 participants <- read.csv2(file = "data/participants.csv")
 
 for (i in 1:nrow(participants)){
@@ -14,9 +20,9 @@ for (i in 1:nrow(participants)){
                       p2 = participants$imie_w[i],
                       p3 = participants$warsztat_nazwa[i]))
   
-  email <- send.mail(from = uzupelnic,
-                     to   = uzupelnic,
-                     subject = "Potwierdzenie rejestracji na warsztaty",
+  email <- send.mail(from = email_from,
+                     to   = email_to,
+                     subject = subject,
                      body = "docs/mail_content.html",
                      html = TRUE,
                      encoding = "utf-8",
