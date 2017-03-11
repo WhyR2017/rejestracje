687a30585df0676919ecfc3faf3ba159042b66e7
krzyslom <krzysztofslomczynski@gmail.com>
2017-03-11 22:14:14 +0100

    Kodowanie utf-8, jeden parametr - data frame

--- R/mail_merge.R
+++ R/mail_merge.R
@@ -1,40 +1,66 @@
+library(dplyr)
 library(knitr)
-library(rmarkdown)
 # devtools::install_github("rpremraj/mailR") # wymaga instalacji java jdk 32bit
 library(mailR)
+library(pander)
+library(readr)
+library(rmarkdown)
 
+# Mail vars
 email_from <- ""
-email_to   <- ""
-subject <- "Potwierdzenie rejestracji na warsztaty"
+subject <- "Potwierdzenie rejestracji na konferencję"
 login_email <- "xxx@gmail.com"
 login_pass  <- "xxx"
 
-participants <- read.csv2(file = "data/participants.csv")
+# Frame vars
+currSheet <- read_csv("link_to_spreadsheet")
+# Classes of attributes
+# sapply(currSheet, class) %>% unname() %>% strtrim(1) %>% paste0(collapse = "")
+prevSheet <- read_csv("data/prevSheet.csv",
+                      col_types = "ccccccccccccccccnnccccc")
+diffSheet <- setdiff(currSheet, prevSheet) %>%
+  rename(`Stopień naukowy` = `Stopień naukowy do umieszczenia na certyfikacie uczestnictwa w konferencji (o ile dotyczy)`,
+         `Udział w konferencji` = `Czy chcesz uczestniczyć w konferencji?`,
+         `Rezygnacja z cateringu` = `Zaznacz poniżej jeżeli chcesz zrezygnować z opłaty cateringowej (10 zł)`,
+         `Warsztaty poranny` = `W którym warsztacie porannym chciałabyś/chciałbyś uczestniczyć 27.09?`,
+         `Warsztat popołudniowy` = `W którym warsztacie popołudniowym chciałabyś/chciałbyś uczestniczyć 27.09?`,
+         `Data urodzin` = `Proszę podaj swoją datę urodzin`) %>%
+  select(Imię, Nazwisko, `Afiliacja / Firma`, `Stopień naukowy`, Email,
+         `Udział w konferencji`, `Rezygnacja z cateringu`, `Warsztaty poranny`,
+         `Warsztat popołudniowy`, `Obecne stanowisko`, `Data urodzin`, Płeć,
+         Kraj, Miasto) %>%
+  mutate(`Rezygnacja z cateringu` = if_else(is.na(`Rezygnacja z cateringu`),
+                                            true = "Nie",
+                                            false = "Tak"))
 
-for (i in 1:nrow(participants)){
+bodyList <- apply(diffSheet, 1, function(row) {
+  data_frame(Formularz = names(row), Dane = unname(row))
+})
+
+mailNumber <- which(bodyList[[1]]$Formularz == "Email")
+
+for (i in 1:length(bodyList)){
   rmarkdown::render(input = "docs/mail_content.Rmd",
                     output_format = "html_document",
                     output_dir = "docs/",
-                    params = list(
-                      p1 = participants$plec[i],
-                      p2 = participants$imie_w[i],
-                      p3 = participants$warsztat_nazwa[i]))
+                    params = list(form = bodyList[[i]][-mailNumber, ]),
+                    encoding = "utf-8")
   
-  email <- send.mail(from = email_from,
-                     to   = email_to,
-                     subject = subject,
-                     body = "docs/mail_content.html",
-                     html = TRUE,
-                     encoding = "utf-8",
-                     smtp = list(host.name = "smtp.gmail.com", 
-                                 port = 465, 
-                                 user.name = login_email, 
-                                 passwd = login_pass, 
-                                 ssl = TRUE),
-                     authenticate = TRUE,
-                     send = FALSE)
+  email <- mailR::send.mail(from = email_from,
+                            to = bodyList[[i]]$Dane[mailNumber],
+                            subject = subject,
+                            body = "docs/mail_content.html",
+                            html = TRUE,
+                            encoding = "utf-8",
+                            smtp = list(host.name = "smtp.gmail.com",
+                                        port = 465,
+                                        user.name = login_email,
+                                        passwd = login_pass,
+                                        ssl = TRUE),
+                            authenticate = TRUE,
+                            send = FALSE)
   
-  # email$addBcc(c(""))
-  # email$addReplyTo(c(""))
   email$send()
 }
+
+write_csv(currSheet, "data/prevSheet.csv")
